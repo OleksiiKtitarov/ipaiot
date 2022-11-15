57d1c2b0d4e4eb1ddd906e3a96a8a922ee4653cc
OleksiiKtitarov <114644398+OleksiiKtitarov@users.noreply.github.com>
2022-11-15 11:57:50 +0100

    Update homework.php

--- homework.php
+++ homework.php
@@ -8,7 +8,7 @@
 </head>
 <body>
     <main>
-        <?php if ($_SERVER['REQUEST_METHOD'] === 'GET') : ?>
+       
             <form action="<?php htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="post">
                 <div>
                     <label for="name">Name:</label>
@@ -20,34 +20,26 @@
                     <input type="email" name="email" required="required" placeholder="Enter your email" />
                 </div>
 
-                <button type="submit">Subscribe</button>
+                <input type="submit" value="Submit">
 
             </form>
-        <?php else : ?>
+        
 
             <?php
         $file = 'results.txt';
-            //---------------------------------------------
-            // WARNING: this doesn't include sanitization
-            // and validation
-            //---------------------------------------------
-            if (isset($_POST['name'], $_POST['email'])) {
-                $name = htmlspecialchars($_POST['name']);
-                $email = htmlspecialchars($_POST['email']);
+            
+                $name = $_POST['name'];
+                $email = $_POST['email'];
                 
                 
                 file_put_contents($file, $name.' '.$email);
 
                 // show the $name and $email
-                echo "Thanks $name for your subscription.<br>";
-                echo "Please confirm it in your inbox of the email $email.";
-            } else {
-                echo 'You need to provide your name and email address.';
-            }
-            echo file_get_contents($file);
+               
+                echo file_get_contents($file);
             ?>
 
-        <?php endif ?>
+        
     </main>
 </body>
 
