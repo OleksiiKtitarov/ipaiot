d2798d9dccb8154b6839ec6ce302cbef2aafe871
OleksiiKtitarov <114644398+OleksiiKtitarov@users.noreply.github.com>
2022-11-15 11:49:40 +0100

    Update homework.php

--- homework.php
+++ homework.php
@@ -33,9 +33,6 @@
             if (isset($_POST['name'], $_POST['email'])) {
                 $name = htmlspecialchars($_POST['name']);
                 $email = htmlspecialchars($_POST['email']);
-//                 $file = 'results.txt';
-                
-//                 file_put_contents($file, $name, $email, FILE_APPEND);
 
                 // show the $name and $email
                 echo "Thanks $name for your subscription.<br>";
