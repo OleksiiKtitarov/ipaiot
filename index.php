599b0127f30924a19ec4416f33d18d0d0c9a46df
OleksiiKtitarov <114644398+OleksiiKtitarov@users.noreply.github.com>
2022-11-16 10:23:47 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,66 +7,66 @@
     <title>PHP Form</title>
 </head>
 <body>
-    <main>
-            <?
-            $file = 'results.txt';
-            echo("Last enterred information 3".file_get_contents($file));
-        ?>
-            <form method="post">
-            <div class="box-shadow">
-                   <div>
-                       <label for="name">Name:</label>
-                       <input type="text" name="name" required="required" placeholder="First name" />
+<main>
+    <?
+    $file = 'results.txt';
+    echo("Last enterred information 4".file_get_contents($file));
+    ?>
+    <form method="post">
+        <div class="box-shadow">
+            <div>
+                <label for="name">Name:</label>
+                <input type="text" name="name" required="required" placeholder="First name" />
 
-                       <label for="surname">Surname:</label>
-                       <input type="text" name="surname" required="required" placeholder="Last Name" />
-                   </div>
-
-                   <div>
-                       <label for="name">Email:</label>
-                       <input type="email" name="email" required="required" placeholder="Enter your email" />
-                   </div>
-
-                   <div>
-                       <label for="phone">Phone:</label>
-                           <input type="phone" name="phone" placeholder="your phone" />
-                   </div>
-
-                   <div>
-                       <input list="topics"  class="datalist_topics" name="Key words" id="topics" placeholder="Choose topic">
-                                       <datalist id="topics">
-                                           <option value="Page trouble">
-                                           <option value="Suggestion">
-                                           <option value="Improvement">
-                                       </datalist>
-                   </div>
-
-                   <div>
-                       <label for="message">Message:</label>
-                           <textarea name="response" id="message" placeholder="Your response"></textarea><br>
-                   </div>
-
-                   <button type="submit" class="buttons_form_class">Send</button>
-
+                <label for="surname">Surname:</label>
+                <input type="text" name="surname" required="required" placeholder="Last Name" />
             </div>
 
+            <div>
+                <label for="name">Email:</label>
+                <input type="email" name="email" required="required" placeholder="Enter your email" />
+            </div>
 
-            </form>
+            <div>
+                <label for="phone">Phone:</label>
+                <input type="phone" name="phone" placeholder="your phone" />
+            </div>
+
+            <div>
+                <input list="topics"  class="datalist_topics" name="Key words" id="key_words" placeholder="Choose topic">
+                <datalist id="topics">
+                    <option value="Page trouble">
+                    <option value="Suggestion">
+                    <option value="Improvement">
+                </datalist>
+            </div>
+
+            <div>
+                <label for="message">Message:</label>
+                <textarea name="response" id="message" placeholder="Your response"></textarea><br>
+            </div>
+
+            <button type="submit" class="buttons_form_class">Send</button>
+
+        </div>
 
 
-            <?php
+    </form>
 
 
-                $name = $_POST['name'];
-                $surname = $_POST['surname'];
-                $phone = $_POST['phone'];
-                $email = $_POST['email'];
-                $topics = $_POST['topics'];
-                $message = $_POST['message'];
+    <?php
+
+
+    $name = $_POST['name'];
+    $surname = $_POST['surname'];
+    $phone = $_POST['phone'];
+    $email = $_POST['email'];
+    $topics = $_POST['key_words'];
+    $message = $_POST['message'];
 
 
 
-                file_put_contents($file, $name.' '.$surname.' '.$topics.' '.$phone.' '.$email);
+    file_put_contents($file, $name.' '.$surname.' '.$topics.' '.$phone.' '.$email);
 
                 // show the $name and $email
 
@@ -74,7 +74,7 @@
             ?>
 
 
-    </main>
+</main>
 </body>
 
 </html>
