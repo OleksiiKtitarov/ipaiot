a33fd167129a4ed5b176042ebfd05e29f96a4e22
OleksiiKtitarov <114644398+OleksiiKtitarov@users.noreply.github.com>
2022-11-16 10:14:15 +0100

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <main>
             <?
             $file = 'results.txt';
-            echo("Last enterred information".file_get_contents($file));
+            echo("Last enterred information 3".file_get_contents($file));
         ?>
             <form action="homework.php" method="post">
             <div class="box-shadow">
