2528eda7ad3e7910360f2f59a3fefbcbb827fa21
OleksiiKtitarov <114644398+OleksiiKtitarov@users.noreply.github.com>
2022-11-16 10:33:42 +0100

    Update index.php

--- index.php
+++ index.php
@@ -66,7 +66,7 @@
 
 
 
-    file_put_contents($file, $name.' '.$surname.' '.$topics.' '.$phone.' '.$email);
+    file_put_contents($file, $name.' '.$surname.' '.$topics.' '.$phone.' '.$email, FILE_APPEND);
 
                 // show the $name and $email
 
