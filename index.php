60b2c928a10d669f7a1d0d06c8923a30e13b7bf1
OleksiiKtitarov <114644398+OleksiiKtitarov@users.noreply.github.com>
2022-11-16 11:42:09 +0100

    Update index.php

--- index.php
+++ index.php
@@ -10,35 +10,31 @@
 <main>
     <div class="article_news">
 
-    <div class="box-shadow">
-        <div style="">
-            <h1 style="text-align: center">Leave your response</h1>
-            <form id="opnFrm" class="opnFrm">
-                
-                <input type="text" name="nick" id="nickname" placeholder="Nick" class="form_input"><br>
-                <input type="text" name="name" id="name" placeholder="Name" class="form_input"><br>
-                <input type="text" name="surname" id="surname" placeholder="Surname" class="form_input"><br>
-                <input type="email" name="email" id="email" placeholder="your@mail" class="form_input"><br>
-                <input type="text" name="number" id="number" placeholder="your number" class="form_input"><br>
-                
-                <input list="topics"  class="datalist_topics" name="Key words" id="key_words" placeholder="Choose topic">
-                <datalist id="topics">
-                    <option value="Page trouble">
-                    <option value="Suggestions">
-                    <option value="Improvements">
-                </datalist>
-                
-                <div class="response_form">
-                    <textarea name="response" id="response" placeholder="Your response"></textarea><br>
-                </div>
+        <div class="box-shadow">
+            <div style="">
+                <h1 style="text-align: center">Leave your response</h1>
+                <form id="opnFrm" class="opnFrm">
 
-                <div class="buttons_form">
-                    <button type="submit" class="buttons_form_class">Send</button>
-                </div>
-            </form>
+                    <input type="text" name="nick" id="nickname" placeholder="Nick" class="form_input"><br>
+                    <input type="text" name="name" id="name" placeholder="Name" class="form_input"><br>
+                    <input type="text" name="surname" id="surname" placeholder="Surname" class="form_input"><br>
+                    <input type="email" name="email" id="email" placeholder="your@mail" class="form_input"><br>
+                    <input type="text" name="number" id="number" placeholder="your number" class="form_input"><br>
+
+                    <input type="text"  class="datalist_topics" name="Key words" id="topics" placeholder="Choose topic">
+                    
+
+                    <div class="response_form">
+                        <textarea name="response" id="response" placeholder="Your response"></textarea><br>
+                    </div>
+
+                    <div class="buttons_form">
+                        <button type="submit" class="buttons_form_class">Send</button>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
-</div>
     <br><div class=article_news">
         <div class="box-shadow">
             <div>
@@ -58,7 +54,7 @@
 
     $name = $_POST['name'];
     $surname = $_POST['surname'];
-    $phone = $_POST['phone'];
+    $phone = $_POST['number'];
     $email = $_POST['email'];
     $topics = $_POST['topics'];
     $message = $_POST['message'];
