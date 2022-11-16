1623c222a5b5b4d7e8fb68bcb4a000f3a1f04fa2
OleksiiKtitarov <114644398+OleksiiKtitarov@users.noreply.github.com>
2022-11-16 11:38:04 +0100

    Update index.php

--- index.php
+++ index.php
@@ -8,47 +8,37 @@
 </head>
 <body>
 <main>
-    <form method="post">
-        <div class=article_news">
-            <div class="box-shadow">
-                <div style="align-content: center">
-                    <br><label for="name">Name:</label>
-                    <input class="form_input" type="text" name="name" required="required" placeholder="First name" />
+    <div class="article_news">
 
-                    <br><label for="surname">Surname:</label>
-                    <input class="form_input" type="text" name="surname" required="required" placeholder="Last Name" /><br>
+    <div class="box-shadow">
+        <div style="">
+            <h1 style="text-align: center">Leave your response</h1>
+            <form id="opnFrm" class="opnFrm">
+                
+                <input type="text" name="nick" id="nickname" placeholder="Nick" class="form_input"><br>
+                <input type="text" name="name" id="name" placeholder="Name" class="form_input"><br>
+                <input type="text" name="surname" id="surname" placeholder="Surname" class="form_input"><br>
+                <input type="email" name="email" id="email" placeholder="your@mail" class="form_input"><br>
+                <input type="text" name="number" id="number" placeholder="your number" class="form_input"><br>
+                
+                <input list="topics"  class="datalist_topics" name="Key words" id="key_words" placeholder="Choose topic">
+                <datalist id="topics">
+                    <option value="Page trouble">
+                    <option value="Suggestions">
+                    <option value="Improvements">
+                </datalist>
+                
+                <div class="response_form">
+                    <textarea name="response" id="response" placeholder="Your response"></textarea><br>
                 </div>
 
-                <div>
-                    <label for="name">Email:</label>
-                    <input class="form_input" type="email" name="email" required="required" placeholder="Enter your email" />
+                <div class="buttons_form">
+                    <button type="submit" class="buttons_form_class">Send</button>
                 </div>
-
-                <div>
-                    <label for="phone">Phone:</label>
-                    <input class="form_input" type="number" name="phone" placeholder="your phone" />
-                </div>
-
-                <div>
-                    <input type="text"  class="datalist_topics" name="Key words" id="key_words" placeholder="Choose topic">
-                    <datalist id="topics">
-                        <option value="Page trouble">
-                        <option value="Suggestion">
-                        <option value="Improvement">
-                    </datalist>
-                </div>
-
-                <div>
-                    <label for="message">Message:</label>
-                    <textarea name="response" id="message" placeholder="Your response"></textarea><br>
-                </div>
-
-                <button type="submit" class="buttons_form_class">Send</button>
-
-            </div>
+            </form>
         </div>
-
-    </form>
+    </div>
+</div>
     <br><div class=article_news">
         <div class="box-shadow">
             <div>
