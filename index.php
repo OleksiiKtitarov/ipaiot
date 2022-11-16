9efdc7d78279d91e3f9d1ac5ca0176215c6ad19d
OleksiiKtitarov <114644398+OleksiiKtitarov@users.noreply.github.com>
2022-11-16 10:16:20 +0100

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
             $file = 'results.txt';
             echo("Last enterred information 3".file_get_contents($file));
         ?>
-            <form action="homework.php" method="post">
+            <form method="post">
             <div class="box-shadow">
                    <div>
                        <label for="name">Name:</label>
@@ -66,7 +66,7 @@
 
 
 
-                file_put_contents($file, $name.' '.$surname.' '.$phone' '.$email.' '.$topics, $message);
+                file_put_contents($file, $name.' '.$surname.' '.$phone.' '.$email);
 
                 // show the $name and $email
 
