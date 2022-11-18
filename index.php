ac5ac28c52c2d16bf3fdb449fc4fd5f4a43cf2e1
OleksiiKtitarov <114644398+OleksiiKtitarov@users.noreply.github.com>
2022-11-18 23:48:32 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>All articles</title>
-    <link rel="stylesheet" href="screenStyle_php1.css">
+    <link rel="stylesheet" href="screenStyle_php.css">
 
 </head>
 <body>
