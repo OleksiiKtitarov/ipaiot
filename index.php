c72d290fca2013a4d85be35127f859d0fb2f590d
OleksiiKtitarov <114644398+OleksiiKtitarov@users.noreply.github.com>
2022-11-18 21:25:35 +0100

    Update index.php

--- index.php
+++ index.php
@@ -56,7 +56,9 @@
 
             $file_nick = 'nick.txt';
             $lines_nick = file($file_nick);//file in to an array
-
+                           
+            echo("Last entered information 3 :\n".file_get_contents($file));
+                           
             echo("Last entered information 4 :\n".file_get_contents($file_nick));
 
             $file_name = 'name.txt';
