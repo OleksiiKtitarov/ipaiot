ae7d0f05b769c186e1047ec3182b6e1748161e0b
OleksiiKtitarov <114644398+OleksiiKtitarov@users.noreply.github.com>
2022-11-20 15:36:42 +0100

    Update index.php

--- index.php
+++ index.php
@@ -45,7 +45,7 @@
               echo file_get_contents('txt/nicks.txt')."<br>\n";
               echo file_get_contents('txt/names.txt').' '.file_get_contents('txt/surnames.txt')."<br>\n";
               echo file_get_contents('txt/numbers.txt').' '.file_get_contents('txt/emails.txt')."<br>\n";                                  
-              echo file_get_contents('txt/topics.txt')."<br>\n";
+              echo file_get_contents('txt/topicss.txt')."<br>\n";
               echo file_get_contents('txt/responses.txt')."<br>\n";
             ?>
         </div>
