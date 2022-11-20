b3b0c0e13eef29ab463498959b3c749cd8f93355
OleksiiKtitarov <114644398+OleksiiKtitarov@users.noreply.github.com>
2022-11-20 14:46:29 +0100

    Update index.php

--- index.php
+++ index.php
@@ -41,9 +41,9 @@
     <div class="box-shadow">
         <div class="last_response">
             <?php
-              echo file_get_contents('txt/numbers.txt').file_get_contents('txt/emails.txt')."<br>\n";
               echo file_get_contents('txt/nicks.txt')."<br>\n";
-              echo file_get_contents('txt/names.txt').file_get_contents('txt/surnames.txt')."<br>\n";
+              echo file_get_contents('txt/names.txt').' '.file_get_contents('txt/surnames.txt')."<br>\n";
+              echo file_get_contents('txt/numbers.txt').' '.file_get_contents('txt/emails.txt')."<br>\n";                                  
               echo file_get_contents('txt/topics.txt')."<br>\n";
               echo file_get_contents('txt/responses.txt')."<br>\n";
             ?>
